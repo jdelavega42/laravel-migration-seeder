eaf7d94f7073e303655b2160d8b6582157c2746c
“jdelavega94” <jcdelavega94@gmail.com>
2023-05-25 17:49:45 +0200

    first commit

---
+++ database/migrations/2023_05_25_153452_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 20);
+            $table->string('departure_station', 40);
+            $table->string('arrival_station', 40);
+            $table->dateTime('estimated_departure');
+            $table->dateTime('estimated_arrival');
+            $table->string('train_code', 10);
+            $table->smallInteger('N_coach')->unsigned();
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
